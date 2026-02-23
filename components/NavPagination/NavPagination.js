d4e74a5920b9ebe9dbeaa646d0f544b2d37cca80
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-02-23 11:06:05 +0100

    refactoring your code

---
+++ components/NavPagination/NavPagination.js
@@ -0,0 +1,5 @@
+export default function NavPagination() {
+    const pagination = document.createElement("span");
+    pagination.classList.add("navigation__pagination");
+    return pagination;
+}
--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import NavButton from "./components/NavButton/NavButton.js";
+import NavPagination from "./components/NavPagination/NavPagination.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
     '[data-js="search-bar-container"]',
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 let maxPage = 1;
 let page = 1;
 let searchQuery = "";
+
+const prevButton = NavButton("previous", () => {
+    if (page > 1) {
+        page--;
+        fetchCharacters();
+    }
+});
+
+const nextButton = NavButton("next", () => {
+    if (page <= maxPage) {
+        page++;
+        fetchCharacters();
+    }
+});
+
+const pagination = NavPagination();
+
+navigation.append(prevButton, pagination, nextButton);
+
+const searchBar = SearchBar((event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    searchQuery = form.elements.query.value;
+    page = 1;
+    fetchCharacters();
+});
+
+searchBarContainer.append(searchBar);
 
 fetchCharacters();
 
@@ -33,26 +61,3 @@ async function fetchCharacters() {
 
     pagination.textContent = `${page} / ${maxPage}`;
 }
-
-prevButton.addEventListener("click", () => {
-    if (page > 1) {
-        page--;
-        fetchCharacters();
-    }
-});
-
-nextButton.addEventListener("click", () => {
-    if (page <= maxPage) {
-        page++;
-        fetchCharacters();
-    }
-});
-
-searchBar.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const form = event.target;
-    searchQuery = form.elements.query.value;
-    page = 1;
-    fetchCharacters();
-});
